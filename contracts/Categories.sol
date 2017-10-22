93cc9375a63cc165cc2bb05bb1afa5e5b663b3d6
Medha Kothari <medhakothari@berkeley.edu>
2017-10-22 02:21:43 -0700

    DO IT

--- contracts/Categories.sol
+++ contracts/Categories.sol
@@ -2,13 +2,14 @@ pragma solidity ^0.4.11;
 
 /// @title A particular categeory for a hackthon with a submission mechanism.
 contract Categories {
-
-//REMEMBER TO DO IPFS!!!!!!!!!!!
     uint _bondAmount;
     uint _rewardAmount;
     uint _endTime;
     address _creator;
     bool isVotingTime;
+    string creatorChoice;
+    string[] subNameList;
+    address[] winners;
 
     struct Submission {
         bytes32 IPFShash; //hash of IPFS hash
@@ -18,13 +19,10 @@ contract Categories {
         address[] retrieved; //list of all contributors who withdrew their bounty and/or bond amount
         uint upVotes; //amount of upvotes
         uint downVotes; //amount of downVotes
-        mapping voted; // list of addresses who already voted (up or down) for this submission
+        mapping voted; // mapping of addresses who already voted (up or down) for this submission
     }
 
     mapping(string => Submission) public submissionList; // holds all submissions for this category
-    mapping(address => bool) public votedList; // who voted tracker
-
-
 
     modifier checkBondPaid () {
         require (msg.value >= bondAmount);
@@ -42,6 +40,7 @@ contract Categories {
         this._endTime = endTime;
         this._creator = creator;
         this.isVotingTime = false; //voting period is not open yet
+        this.subNameList = new string[];
         return this;
     }
 
@@ -58,7 +57,10 @@ contract Categories {
             submissionList[submissionName].upVotes = 0;
             submissionList[submissionName].downVotes = 0;
 
-            submissionList[submissionName].voted = new address[];
+            mapping(address => bool) public votedList; // who voted tracker
+            submissionList[submissionName].voted = votedList;
+            this.subNameList.push(subName);
+
         }
     }
 
@@ -74,23 +76,47 @@ contract Categories {
         require(submissionList[subName].voted > 0);
         timeToVote();
         require(isVotingTime);
-        // require(subName in submissionList);
-        if (!votedList[msg.sender]) {
-            votedList[msg.sender] = true;
-            if (vote == -1) {
-                submissionList[subName].downVotes += 1;
-            } else {
-                submissionList[subName].upVotes += 1;
-            }
+        if (msg.sender == _creator) {
+            this.creatorChoice = subName;
+            submissionList[subName].upVotes += this.subNameList.length;
+        } else {
+            if (!submissionList[subName].voted[msg.sender]) {
+                submissionList[subName].voted[msg.sender] = true;
+                if (vote == -1) {
+                    submissionList[subName].downVotes += 1;
+                } else {
+                    submissionList[subName].upVotes += 1;
+                }
 
-        } 
+            } 
+        }
     }
 
-    function Round() {
-        //voting is allowed now (do the boolean thang)
+    function Round() public returns (string winnerSubName) {
+        //voting is not allowed now (do the boolean thang)
         //call superScore??, calculate popular vote, return total score and winner
         //draw??
         this.isVotingTime = false; //prevents others from voting; voting period is closed
+        string subWinner;
+        uint trackMaxScore = 0;
+        for (uint i = 0; i < this.subNameList.length; i++) {
+            string tempSub = subNameList[i];
+            uint ups = submissionList[tempSub].upVotes;
+            uint downs = submissionList[tempSub].downVotes;
+            uint net = ups - downs;
+            if (net > trackMaxScore) {
+                trackMaxScore = net;
+                subWinner = tempSub;
+            } else if (net == trackMaxScore) { //tiebreaker
+                if (submissionList[subWinner].upVotes < submissionList[tempSub].upVotes) {
+                    trackMaxScore = net;
+                    subWinner = tempSub;
+                }
+            }
+        }
+        this.winners = submissionList[subWinner].contributors;
+        return subWinner;
+    
     }
 
     function withdraw(string subName) public {
