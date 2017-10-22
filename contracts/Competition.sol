67b6ce6fb858aef0b90613be3e5925702ac4cc18
Noah Alcus <ntalcus@berkeley.edu>
2017-10-21 18:24:25 -0700

    hi medha

--- contracts/Competition.sol
+++ contracts/Competition.sol
@@ -1,17 +1,25 @@
 pragma solidity ^0.4.11;
+import Categories.sol;
+
 
 contract Competition {
 	// Parameters related to the instantiation of the comepetition which will run for a certain amount of time
 	// The competition 
 	address public host;
 	string public competitionName;
+	uint public submissionEnd;
 	uint public competitionEnd;
+
+	// Mapping of categories to owners and addresses
+	mapping(string => owner) catInfo;
 
 	//Current state of competition.
 	State status;
 
 	//Bool related to competition status. If competition underway, False, otherwise True;
 	bool running;
+
+	// Modifiers that check the status
 
 	//Events that mark certain times.
 	event Commenced(address compCreator);
@@ -25,5 +33,16 @@ contract Competition {
 	competitionEnd = now + _competitionTime;
 	}
 
+	function createCategory(address catOwner, string catName, uint bond) {
+		if (bond <= 0 || catName.length == 0) {
+			throw;
+		}
+
+		// insert constructor here
+		CategoryCreated(catOwner, catName, bond);
+
+	}
+
+	function 
 	
 }
